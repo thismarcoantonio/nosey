d637046c1076920a474643e3316469008b6a2a77
thismarcoantonio <thismarcoantonio@gmail.com>
2026-04-18 09:37:22 -0700

    Add tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,21 @@
     documentation
   </p>
 </template>
+
+<style>
+@import 'tailwindcss';
+
+@theme {
+  --color-primary-50: #fff0f2;
+  --color-primary-100: #ffe0e5;
+  --color-primary-200: #ffc0cc;
+  --color-primary-300: #ff8fa3;
+  --color-primary-400: #ff5175;
+  --color-primary-500: #ff1a4b;
+  --color-primary-600: #e8002f;
+  --color-primary-700: #c10026;
+  --color-primary-800: #a00024;
+  --color-primary-900: #850024;
+  --color-primary-950: #4b000f;
+}
+</style>
