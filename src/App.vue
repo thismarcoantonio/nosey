259a5a55ea80d00692d437d7b6eefd2e2b7cb3f0
thismarcoantonio <thismarcoantonio@gmail.com>
2026-05-12 20:06:19 -0700

    Add primary colour variations

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,17 @@
   --color-canvas: #faf3df;
   --color-ochre: #cd9a3a;
   --color-dusty-rose: #eddcd3;
+  --color-primary-50: #edf5eb;
   --color-primary-100: #a7c89d;
+  --color-primary-200: #8fb98a;
   --color-primary-300: #7ea475;
+  --color-primary-400: #688f5f;
+  --color-primary-500: #547a4c;
+  --color-primary-600: #426239;
+  --color-primary-700: #324b2b;
+  --color-primary-800: #22341e;
+  --color-primary-900: #141f12;
+  --color-primary-950: #0d150b;
 }
 
 body {
